f7ba3c8109d4f6828da3d7d1af935fbe23ce17ec
ArTuR00232 <aopnascimento@sga.pucminas.br>
2022-09-12 16:23:38 -0300

    Update src.js

--- codigos/src/src.js
+++ codigos/src/src.js
@@ -29,30 +29,14 @@ function criausuario(id,username, password,age)
   
 function checar()
 {
-    let idade = document.getElementById("input-idade").value
     let username = document.getElementById("input-nome").value
     let idade = document.getElementById("input-idade").value
     let senha = document.getElementById("input-senha").value
     let confirma = document.getElementById("input-confirm").value
      if(username!="")
      {
-<<<<<<< HEAD
         if(idade >= 18)
         {
-        
-            if(senha!="" && senha==confirma)
-            {
-                cadastra()
-            }
-            else
-            {
-                alert("As senhas não são compatíveis")
-            }
-        }
-        else
-        {
-=======
-        if(idade >= 18){
             if(senha!="")
             {
                 if(senha==confirma)
@@ -66,19 +50,16 @@ function checar()
             }
         }
         else{
->>>>>>> 9cf2eeece3ef351e50725c25d5f060c7d9617dfd
             alert("Você deve ser maior de 18 para se cadastrar")
         }
-     }
+        }
      else
      {
          alert("É necessário seu nome para se cadastrar")
      }
 }
-
 function limpar()
 {
    localStorage.clear()
    window.location.href = ("index.html")
 }
-    
