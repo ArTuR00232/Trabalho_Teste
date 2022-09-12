33dc1ce2b64d703929067637ca6b3d530d8fdb7e
ArTuR00232 <aopnascimento@sga.pucminas.br>
2022-09-12 16:19:09 -0300

    Update src.js

--- codigos/src/src.js
+++ codigos/src/src.js
@@ -5,22 +5,22 @@ function cadastra()
 {
     let username =  document.getElementById("input-nome").value
     let password = document.getElementById("input-senha").value
-    let idade = document.getElementById("input-idade").value
+    let age = document.getElementById("input-idade").value
     let ID =  localStorage.length +1;
-    let outra = criausuario(ID,username,password, idade)
+    let outra = criausuario(ID,username,password, age)
     dados = JSON.stringify(outra)
     localStorage.setItem(ID,dados)
     alert("usuario criado")
     window.location.href = ("login.html")
 }
 
-function criausuario(id,username, password,idade)
+function criausuario(id,username, password,age)
 {
     var objeto = {}
     objeto.id = id;
     objeto.username = username;
     objeto.password = password;
-    objeto.idade = idade;
+    objeto.age = age;
 
 
     console.log(objeto)
@@ -29,20 +29,28 @@ function criausuario(id,username, password,idade)
   
 function checar()
 {
+    let idade = document.getElementById("input-idade").value
     let username = document.getElementById("input-nome").value
     let senha = document.getElementById("input-senha").value
     let confirma = document.getElementById("input-confirm").value
      if(username!="")
      {
+        if(idade >= 18)
+        {
         
-         if(senha!="" && senha==confirma)
-         {
-             cadastra()
-         }
-         else
-         {
-             alert("As senhas não são compatíveis")
-         }
+            if(senha!="" && senha==confirma)
+            {
+                cadastra()
+            }
+            else
+            {
+                alert("As senhas não são compatíveis")
+            }
+        }
+        else
+        {
+            alert("Você deve ser maior de 18 para se cadastrar")
+        }
      }
      else
      {
@@ -53,5 +61,6 @@ function checar()
 function limpar()
 {
    localStorage.clear()
+   window.location.href = ("index.html")
 }
     
